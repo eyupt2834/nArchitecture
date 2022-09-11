a5605e3cf9974e5991fffe61c8c6ae6e643daf06
eyup turgut <eyuptrgtt@gmail.com>
2022-09-11 10:43:19 +0300

    Day 4 -Section 1

--- src/demoProjects/rentACar/Application/Features/Brands/Dtos/BrandListDto.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Dtos/BrandListDto.cs
@@ -10,7 +10,7 @@ namespace Application.Features.Brands.Dtos
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
         
     }
 }
