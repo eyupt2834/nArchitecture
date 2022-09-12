503f237b85a232d591f6db97b2820dd181d9e58d
eyup turgut <eyuptrgtt@gmail.com>
2022-09-12 15:59:11 +0300

    Day 4 - Section 4

--- src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -15,6 +15,7 @@ namespace Persistence
             services.AddDbContext<BaseDbContext>(options =>options.UseSqlServer( configuration.GetConnectionString("RentACarCampConnectionString")));
 
             services.AddScoped<IBrandRepository, BrandRepository>();
+            services.AddScoped<IModelRepository, ModelRepository>();
 
             return services;
         }
